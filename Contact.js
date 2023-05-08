70d1aea66c384d0b0cca287a045694be541218b3
kaxxsh <kameshwaran2002@gmail.com>
2023-05-08 11:58:26 +0530

    update

--- Contact.js
+++ Contact.js
@@ -4,26 +4,23 @@ const Contact = Express.Router();
 
 const listenarr = [
   {
-    id: 1,
     name: "kamesh",
   },
   {
-    id: 2,
     name: "sanjay",
   },
   {
-    id: 3,
     name: "shanu",
   },
 ];
 
 Contact.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const data = listenarr.find((items) => items.id == id);
-  console.log(req.params.id);
-  if (data) {
-    res.send(data.name);
-  }
+  res.send(req.service.name);
+});
+
+Contact.param("id", (req, res, next, id) => {
+  req.service = listenarr[id - 1];
+  next();
 });
 
 export default Contact;
